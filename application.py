09d02786ae571d0ed4106430f52910dffb6905a6
Piyush Kumar <153419027+P-yush509@users.noreply.github.com>
2025-07-30 15:27:24 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle
+
+with open('model.pkl', 'rb') as file:
+    model, preprocessor = pickle.load(file)
+
+st.title("Health Insurance Monthly Premium Predictor")
+
+age = st.slider("Age", 18, 100)
+bmi = st.number_input("BMI", 10.0, 50.0)
+children = st.number_input("Children", 0, 10)
+sex = st.selectbox("Sex", ["male", "female"])
+smoker = st.selectbox("Smoker", ["yes", "no"])
+region = st.selectbox("Region", ["northeast", "southeast", "southwest", "northwest"])
+
+if st.button("Predict"):
+    user_df = pd.DataFrame([[sex, smoker, region, age, bmi, children]], 
+                           columns=['sex', 'smoker', 'region', 'age', 'bmi', 'children'])
+    X_input = preprocessor.transform(user_df)
+    prediction = model.predict(X_input)[0]
+    if prediction <= 0:
+        st.error("You Don't Need Insurance")
+    else:
+        st.success(f"Estimated Medical Cost: INR {prediction:.2f}")
+
